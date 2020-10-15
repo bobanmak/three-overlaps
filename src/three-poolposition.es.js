42968a6100682a6803c4b1bcf0e123d420d2092f
Boban Jordanoski <mail@jordanoski.de>
2020-10-15 17:02:53 +0200

    add: find new poolPosition

--- src/three-poolposition.es.js
+++ src/three-poolposition.es.js
@@ -1,4 +1,6 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
+import { Overlaps  } from './three-overlaps.es.js';
+
 
 /**
  * newEL = { width, height, deepth }; // Object3D
@@ -21,33 +23,96 @@ const PoolPosition = function( opts )
 {
     this.info = "Pool-Position";
     this.options = Object.assign( {}, defaults, opts );
+    this.overlap = new Overlaps();
 };
 
 Object.assign( PoolPosition.prototype, {
     
     constructor: PoolPosition,
 
-    findFreeSpace: function( scene, el, opts ){
+    /** Make Vertecies Validation mesh/geometry/vertices
+     * 
+     * https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+     * 
+     * @param {* Ground Mesh - Polygon } availableSpace 
+     * @param {* List with Constraints - Meshes} constraints 
+     * @param {* Element to place} el 
+     * @param {* Additional Options} opts 
+     * 
+     * opts : axis-aligned, separating-axis, default: axis-aligned
+     */
+    findFreeSpace: function( availableSpace, constraints, element, opts, scene ){
 
-        if ( !el ){
-            console.error( "The Element is not defined!", el );
+        let freeSpaceFound = false;
+        let newPositon;
+
+        // Validation        
+        if ( !availableSpace ) return this.options.poolPosition;
+        if ( !element ){
+            console.error( "The Element is not defined!", element );
             return;
         }
-
-        if ( !scene ) return this.options.poolPosition;
-     
-     
-        if ( opts && opts.neighbour ){
-
-        }
+        // Validation ends
+          
+        if ( opts && opts.neighbour ){ }
         
-        if ( opts && opts.entagled ){
+        else if ( opts && opts.entagled ){ }
 
+        else {
+
+            // without rules or other limitation, random method
+            while( !freeSpaceFound ){
+            
+                newPositon = this.generateRandomPosition( availableSpace );
+
+                element.position.copy( newPositon );
+
+                let onGround        = this.overlap.onGround( element, availableSpace );
+                let csIntersection  = this.overlap.testIntersectList( element, constraints );
+
+    
+                if ( !onGround || csIntersection  ){
+                    continue;
+                } else {
+                    freeSpaceFound = true;
+                }
+
+                 
+            }
         }
 
-        return this.options.poolPosition;
+        return newPositon;
 
+    },
+
+    generateRandomPosition: function( limit ){
+        // limit available space to the boundingBox
+       
+        limit.updateMatrixWorld();
+        limit.geometry.computeBoundingBox();
+        let boundingBox = limit.geometry.boundingBox.clone() ;
+        boundingBox.applyMatrix4( limit.matrixWorld );
+
+
+        let x = [ boundingBox.min.x, boundingBox.max.x ];
+        let z = [ boundingBox.min.z, boundingBox.max.z ];
+
+
+        return new THREE.Vector3( this.getRndInteger(x), 0, this.getRndInteger(z));
+    
+    },
+
+    getRndInteger: function( arr ) {
+    
+        //return Math.floor( Math.random() * (arr[1] - arr[0] + 1) ) + arr[0];
+        return Math.floor(arr[0] + Math.random()*(arr[1] + 1 - arr[0]))
+
+    },
+
+    getVertices: function( mesh ) {
+        return this.overlap.getVerticesList( mesh );
     }
+
 
 });
 
--- test/js/testPoolPosition.es.js
+++ test/js/testPoolPosition.es.js
@@ -5,19 +5,14 @@ import Grassground from "./Grassground.es.js";
 import { TransformControls  } from'../../node_modules/three/examples/jsm/controls/TransformControls.js';
 import { PoolPosition  } from'../../src/three-poolposition.es.js';
 
-
-
 (function () {
-    
+
+    // Viewport starts ---
     let VP = new Viewport();
 
     VP.init();
     VP.start();
 
-    let Helper = new PoolPosition();
-    let objects  = [];
-
-    VP.camera.position.z = 500;
 
     // add a ambient light
     VP.scene.add( new THREE.AmbientLight( 0x020202 ) );
@@ -26,52 +21,136 @@ import { PoolPosition  } from'../../src/three-poolposition.es.js';
     let light	= new THREE.DirectionalLight('white', 2);
     light.position.set(100, 100, 300);
     VP.scene.add( light );
+   
+    VP.camera.position.copy( new THREE.Vector3( 120, 2000, 2400) );
+    VP.camera.rotation.copy( new THREE.Euler( -0.67, -2.8, -2.3 ) ) ;
+    // Viewport ends ---
 
-    //box number one
-    let mesh1 = new WoodBox(100, 100, 100);
-    mesh1.name = "box_1";
-    mesh1.position.set(-200, 50, 0);
+    const config = {
+        interval: true,
+        intervalTime: 1000,
+        visualise: true
+    };
 
-    VP.scene.add( mesh1 );
-    makeInteractive( mesh1 );
-
-    //box number two
-    let mesh2 = new WoodBox(110, 80, 65);
-    mesh2.name = "box_2";
-    mesh2.position.set(-300, 50, 0);
-
-    VP.scene.add( mesh2 );
-    makeInteractive( mesh2 );
-
-   let ground = new Grassground({
-        width		: 2000,
-        height		: 2000,
+    let Helper  = new PoolPosition();
+    let constraints = [];
+    let p, mesh;
+    
+    // available space
+    let ground = new Grassground({
+        width		: 2500,
+        height		: 2500,
         repeatX		: 10,
         repeatY		: 10,
         "image" : "big"
     });
 
-    VP.scene.add( ground );   
+    VP.scene.add( ground );
 
-    let newPosition = Helper.findFreeSpace( VP.scene, mesh1, {} );
-    console.log( "newPosition: ", newPosition );
+    let box1 = createBox({ 
+        size        : [500, 500, 500], 
+        rotationY   : .33, 
+        position    : [-800, 0, 0],
+        name: "box1" 
+    });
+
+    let box2 = createBox({ 
+        size        : [400, 500, 500], 
+        rotationY   : .63, 
+        position    : [-100, 0, -800],
+        name: "box2" 
+    });
+
+    let box3 = createBox({ 
+        size        : [100, 500, 1500], 
+        rotationY   : -.63, 
+        position    : [-200, 0, -100],
+        name: "box3"  
+    });
+
+    //makeInteractive( box3 );
+
+    constraints.push( box1, box2, box3 );
+
+    let newBox = createBox({ 
+        size        : [500, 500, 500], 
+        rotationY   : -.33, 
+        name: "newBox"  
+    });
+
+    let newPosition = Helper.findFreeSpace( ground, constraints, newBox, {} );
+    newBox.position.copy( newPosition );
+    
+    if ( config.visualise ) visualiseVertices( Helper.getVertices( newBox ) );
+    if ( config.interval ){
+        setInterval( () => {
+
+            newPosition = Helper.findFreeSpace( ground, constraints, newBox, {} );
+            newBox.position.copy( newPosition );
+            if ( config.visualise ) visualiseVertices( Helper.getVertices( newBox ) );
+
+        }, config.intervalTime );
+    }
+        
+    
+    function visualiseVertices( list ){ 
+        // add cubes on List of Points
+
+        let deletable = VP.scene.getObjectByName( "deletable" );
+
+        if ( typeof deletable === "undefined" ){
+            deletable = new THREE.Group();
+            deletable.name = "deletable";
+            VP.scene.add( deletable );
+        } 
+
+        // delete old markers
+        for (let i = deletable.children.length - 1; i >= 0; i--) {
+            deletable.remove(deletable.children[i]);
+        }
+
+        let geometry = new THREE.BoxGeometry( 50, 50, 50 );
+        let material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+
+        for ( let i= 0; i < list.length ; i++ ){
+            let cube = new THREE.Mesh( geometry, material );
+            cube.position.copy( list[i] );
+            
+            deletable.add( cube );
+        }
+        
+    };
+
+    function createBox( param ){
+        // createBox with parameter and add to scene
+        mesh = new WoodBox( ...param.size );
+        
+        if ( param.rotationY ) mesh.rotation.y = param.rotationY;  
+        if ( param.position ) mesh.position.set( ...param.position );
+
+        p = mesh.geometry.parameters;
+        mesh.geometry.applyMatrix4( new THREE.Matrix4().makeTranslation(  p.depth/2, p.width/2,  p.height/2 ) );
+        mesh.name = "box";
+
+        VP.scene.add( mesh );
+        return mesh;
+    };
 
     function makeInteractive( mesh ){
 
-        objects.push( mesh );
 
         let ctr = new TransformControls( VP.camera, VP.renderer.domElement );
        
         ctr.attach( mesh );
-       
+        
         ctr.addEventListener( 'dragging-changed', function( e ){
             VP.control.enabled = ! e.value 
         });
+    
 
         VP.scene.add( ctr );
 
-    }
-
+    };
    
 })();
 
